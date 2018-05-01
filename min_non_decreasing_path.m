dddc180dd884c3bd100af3113b664f4b19866552
Ambrose <user@nb-infomm17-04.maths.ox.ac.uk>
2018-05-01 11:16:47 +0100

    added notes folder

--- min_non_decreasing_path.m
+++ min_non_decreasing_path.m
@@ -6,7 +6,7 @@ N = 3;
 n_a = 2; %number of actions available to each agent
 S = n_a^N; % total number of actions/states in the system
 C = 1000; % range of payoff; small C, more likely for agents to have the same payoff values
-rng(0);
+rng(1);
 U = uint8(ceil(rand(S, N)*C)); %check correlation! 
 % first index: i \in I set of agents
 % subsequent indices: (a_1, a_2, a_3);
@@ -17,16 +17,16 @@ A = zeros(S,S);
 for i=1:1:S-1
     for j= i+1:1:S
         if M(j) >= M(i)
-            A(i,j) = M(j) - M(i) + 1;
+            A(i,j) = M(j) - M(i) ;
         end
         if M(j) <= M(i)
-            A(j,i) = M(i) - M(j) + 1;
+            A(j,i) = M(i) - M(j) ;
         end
     end
 end
 
 Abin = A ~= 0;
-degree = sum(Abin,1).' + sum(Abin,2)
+degree = sum(Abin,1).' + sum(Abin,2);
 
 %imshow(A);
 G = digraph(A);
